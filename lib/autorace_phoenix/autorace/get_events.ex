bc4215dfd3fa72301ff3857357f21603fd54c59f
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2021-08-01 11:48:57 +0900

    refactor

--- lib/autorace_phoenix/autorace/get_events.ex
+++ lib/autorace_phoenix/autorace/get_events.ex
@@ -1,12 +1,19 @@
 defmodule AutoracePhoenix.Autorace.GetEvents do
   def run do
-    HTTPoison.get!("https://admob-app-id-5452967350.firebaseio.com/events.json")
+    HTTPoison.get("https://admob-app-id-5452967350.firebaseio.com/events.jso")
+    |> handle_response()
     |> Map.get(:body)
     |> Jason.decode!()
     |> Enum.map(fn %{"start" => start, "end" => ending} = map ->
       Map.merge(map, %{"start" => from_unix(start), "end" => from_unix(ending)})
     end)
   end
+
+  defp handle_response({:ok, %{status_code: 200} = response}), do: response
+
+  defp handle_response({:ok, _response}), do: %{body: "[]"}
+
+  defp handle_response({:error, _}), do: %{body: "[]"}
 
   defp from_unix(t) do
     dt = DateTime.from_unix!(div(t, 1000)) |> DateTime.add(60 * 60 * 9)
