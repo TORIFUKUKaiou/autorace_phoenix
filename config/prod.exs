ee313d4662468afa74492ad6ba7b465ba32b7e02
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2021-11-21 03:15:13 +0900

    config/prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :autorace_phoenix, AutoracePhoenixWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: nil, port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
