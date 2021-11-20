10af904b0b721065a6c58274cb5aed11b06f3f7e
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2021-11-21 03:09:44 +0900

    test/autorace_phoenix_web/controllers/page_controller_test.exs

--- test/autorace_phoenix_web/controllers/page_controller_test.exs
+++ test/autorace_phoenix_web/controllers/page_controller_test.exs
@@ -1,8 +1,8 @@
 defmodule AutoracePhoenixWeb.PageControllerTest do
   use AutoracePhoenixWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
+  test "GET /page", %{conn: conn} do
+    conn = get(conn, "/page")
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
 end
