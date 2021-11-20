20f7b89ace091c5b2dbf8887887f0a417944c7f4
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2021-11-21 02:35:51 +0900

    mix deps.get

--- mix.exs
+++ mix.exs
@@ -44,7 +44,9 @@ defmodule AutoracePhoenix.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:zoneinfo, "~> 0.1.4"},
+      {:httpoison, "~> 1.8"}
     ]
   end
 
