7e4a27d685d5d394314d12cb081323e97faac73d
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2021-11-21 02:05:39 +0900

    delete old project

--- .formatter.exs
+++
@@ -1,6 +0,0 @@
-[
-  import_deps: [:ecto, :phoenix, :surface],
-  surface_inputs: ["{lib,test}/**/*.{ex,exs,sface}"],
-  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  subdirectories: ["priv/*/migrations"]
-]
--- lib/autorace_phoenix_web/views/layout_view.ex
+++
@@ -1,3 +0,0 @@
-defmodule AutoracePhoenixWeb.LayoutView do
-  use AutoracePhoenixWeb, :view
-end
